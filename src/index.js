1d7c13c11559a9e91f8e518655d5fb4f883b3256
raulfhaidari <159020035+raulfhaidari@users.noreply.github.com>
2024-04-18 14:53:33 +0200

    rendering mult notes with map function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './components/App'
 
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-ReactDOM.render(
-  <App />,
-document.getElementById("root"))
+root.render(<App  />)
